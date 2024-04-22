6d7704f47a2049feb0a0b98fcc436426cd8110ea
moerwald <andreas.mewald@gmail.com>
2024-04-22 18:22:50 +0200

    Inital commit

---
+++ WpfApp4/IncreaseTextSizeCommand.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp4
+{
+    public abstract class CommandBase : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public virtual bool CanExecute(object? parameter) => true;
+
+        public abstract void Execute(object? parameter);
+
+        public EventHandler OnCanExecute;
+
+        protected void OnCanExecuteChanged()
+        {
+            OnCanExecute?.Invoke(this, EventArgs.Empty);   
+        }
+    }
+
+
+
+    public class IncreaseTextSizeCommand : CommandBase
+
+    {
+        private MainWindowViewModel _viewModel;
+
+        public IncreaseTextSizeCommand(MainWindowViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            _viewModel.IncreaseTextSize();
+        }
+    }
+    public class DecreaseTextSizeCommand : CommandBase
+    {
+        private MainWindowViewModel _viewModel;
+
+        public DecreaseTextSizeCommand(MainWindowViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            _viewModel.DecreaseTextSize();
+        }
+    }
+}
---
+++ WpfApp4/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp4
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private int _fontSize = 14;
+
+        public MainWindowViewModel()
+        {
+            IncreaseTextSizeCommand = new IncreaseTextSizeCommand(this);
+            DecreaseTextSizeCommand = new DecreaseTextSizeCommand(this);
+        }
+
+        public int FontSize
+        {
+            get => _fontSize; set
+            {
+                _fontSize = value;
+                OnPropertyChanged();
+            }
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void IncreaseTextSize() => FontSize += 2;
+        public void DecreaseTextSize() => FontSize -= 2;
+
+        public ICommand IncreaseTextSizeCommand { get;  }
+        public ICommand DecreaseTextSizeCommand { get;  }
+
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
